40f4246e0ddea95ace8b8b168948a1764d3a7310
chiyoup <chiyo.up@gmail.com>
2018-06-10 17:55:59 -0300

    Exercicio 05 Aula 01

--- aula-01/src/Exercicio05.java
+++ aula-01/src/Exercicio05.java
@@ -8,7 +8,7 @@ public class Exercicio05 {
 
     public static void menu() {
         String nome = "Chiyo";
-        System.out.println("Ola " + nome + "! voce tem " + saldo + " disponiveis.");
+        System.out.println("Ola " + nome + "! voce tem " + String.format("%.2f", (double)saldo) + " disponiveis.");
         System.out.println("[1] AK47 Rifles");
         System.out.println("[2] SHOTGUNS");
         System.out.println("[3] SCOUT RIFLE");
@@ -22,29 +22,29 @@ public class Exercicio05 {
 
         switch (numero) {
             case 1:
-                System.out.println("[1] Arsenal AK47 - R$ 900,00");
-                System.out.println("[2] CMMG MK47 Mutant T");
-                System.out.println("[3] Arsenal SLR-106UR AK Pistol");
-                System.out.println("[4] GSG AK-47 22 Tribute Rebel Furniture");
-                System.out.println("[5] Century Arms Romanian GP WASR AK-47");
+                System.out.println("[1] Arsenal AK47 - R$ 1.484,09");
+                System.out.println("[2] CMMG MK47 Mutant T - R$ 1.230,89");
+                System.out.println("[3] Arsenal SLR-106UR AK Pistol - R$ 905,99");
+                System.out.println("[4] GSG AK-47 22 Tribute Rebel Furniture - R$ 410,79");
+                System.out.println("[5] Century Arms Romanian GP WASR AK-47 - R$ 679,99");
                 System.out.println("[6] Voltar para o menu principal");
                 menu3();
                 break;
             case 2:
-                System.out.println("[7] Mossberg 590 Shockwave 410 Ga");
-                System.out.println("[8] Kel-Tec KSG Tactical Pump Shotgun 12 Ga");
-                System.out.println("[9] Standard Manufacturing DP-12 Pump 12 Ga");
-                System.out.println("[10] Remington Model 870 Tac-14 20g");
-                System.out.println("[11] Stoeger Double Defense");
+                System.out.println("[7] Mossberg 590 Shockwave 410 Ga - R$ 349,99");
+                System.out.println("[8] Kel-Tec KSG Tactical Pump Shotgun 12 Ga - R$ 915,19");
+                System.out.println("[9] Standard Manufacturing DP-12 Pump 12 Ga - R$ 1.060,99");
+                System.out.println("[10] Remington Model 870 Tac-14 20g - R$ 341,99");
+                System.out.println("[11] Stoeger Double Defense - R$ 409,99");
                 System.out.println("[12] Voltar para o menu principal");
                 menu3();
                 break;
             case 3:
-                System.out.println("[13] Savage Model 11 Scout");
-                System.out.println("[14] Ruger Gunsite Scout Rifle, 308, W/Detachable Magazine");
-                System.out.println("[15] Springfield Armory M1A Scout Squad SA 308 Win 18 10+1 Syn Stk Black");
-                System.out.println("[16] Ruger Gunsite Scout Rifle, 308, Matte SS, 10 Round Mag 18");
-                System.out.println("[17] Springfield Armory M1A Scout Squad SA 308 Win 18 10+1 Walnut Stk Blue");
+                System.out.println("[13] Savage Model 11 Scout - R$ 665,89");
+                System.out.println("[14] Ruger Gunsite Scout Rifle, 308, W/Detachable Magazine - R$ 868,99");
+                System.out.println("[15] Springfield Armory M1A Scout Squad SA 308 Win 18 10+1 Syn Stk Black - R$ 1.562,39");
+                System.out.println("[16] Ruger Gunsite Scout Rifle, 308, Matte SS, 10 Round Mag 18 - R$ 916,49");
+                System.out.println("[17] Springfield Armory M1A Scout Squad SA 308 Win 18 10+1 Walnut Stk Blue - R$ 1.668,09");
                 System.out.println("[18] Voltar para o menu principal");
                 menu3();
                 break;
@@ -57,28 +57,28 @@ public class Exercicio05 {
         System.out.println("Escolha uma opcao: ");
         Scanner scanner = new Scanner(System.in);
         int numero = scanner.nextInt();
+        double x = 0;
 
         switch (numero){
             case 1:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
-                menu();
+                x = 1484.00;
+                verificaSaldo(saldo, x);
                 break;
             case 2:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 1230.89;
+                verificaSaldo(saldo, x);
                 break;
             case 3:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 905.99;
+                verificaSaldo(saldo, x);
                 break;
             case 4:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 410.79;
+                verificaSaldo(saldo, x);
                 break;
             case 5:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 679.99;
+                verificaSaldo(saldo, x);
                 break;
             case 6:
             case 12:
@@ -86,45 +86,55 @@ public class Exercicio05 {
                 menu();
                 break;
             case 7:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 349.99;
+                verificaSaldo(saldo, x);
                 break;
             case 8:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 915.19;
+                verificaSaldo(saldo, x);
                 break;
             case 9:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 1060.99;
+                verificaSaldo(saldo, x);
                 break;
             case 10:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 341.99;
+                verificaSaldo(saldo, x);
                 break;
             case 11:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 409.99;
+                verificaSaldo(saldo, x);
                 break;
             case 13:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 665.89;
+                verificaSaldo(saldo, x);
                 break;
             case 14:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 868.99;
+                verificaSaldo(saldo, x);
                 break;
             case 15:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 1562.39;
+                verificaSaldo(saldo, x);
                 break;
             case 16:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 916.49;
+                verificaSaldo(saldo, x);
                 break;
             case 17:
-                saldo -= 900.00;
-                System.out.println("Obrigado pela compra! Seu novo saldo e " + saldo);
+                x = 1668.09;
+                verificaSaldo(saldo, x);
                 break;
+        }
+    }
+
+    public static void verificaSaldo(double saldo, double valor){
+        double x = valor;
+        if (saldo < x ) {
+            System.out.println("Saldo insuficiente");
+        } else {
+            saldo -= x;
+            System.out.println("Obrigado pela compra! Seu novo saldo e " + String.format("%.2f", (double)saldo));
         }
     }
 }
